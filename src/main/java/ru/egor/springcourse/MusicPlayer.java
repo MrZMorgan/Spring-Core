d50d5c3646901a27661038171f2a0f2f2a163eea
Egor Anchutin <zbit16@gmail.com>
2020-12-04 20:27:49 +0300

    Learn more about Dependency Injection

--- src/main/java/ru/egor/springcourse/MusicPlayer.java
+++ src/main/java/ru/egor/springcourse/MusicPlayer.java
@@ -3,7 +3,34 @@ package ru.egor.springcourse;
 public class MusicPlayer {
     private Music music;
 
+    private String name;
+    private int volume;
+
+    //IoC
+    public MusicPlayer() {
+    }
+
     public MusicPlayer(Music music) {
+        this.music = music;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    public void setMusic(Music music) {
         this.music = music;
     }
 
