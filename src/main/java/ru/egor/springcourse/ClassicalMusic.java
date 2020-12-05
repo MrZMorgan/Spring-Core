b3829ebb564a534caa7bf09268fed86e0db68f00
Egor Anchutin <zbit16@gmail.com>
2020-12-05 21:14:51 +0300

    Learn about @Scope, @Value, @PostConstruct, @PreDestroy

--- src/main/java/ru/egor/springcourse/ClassicalMusic.java
+++ src/main/java/ru/egor/springcourse/ClassicalMusic.java
@@ -1,9 +1,22 @@
 package ru.egor.springcourse;
 
 import org.springframework.stereotype.Component;
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 
 @Component
 public class ClassicalMusic implements Music {
+
+    @PostConstruct
+    public void doMyInit(){
+        System.out.println("Doing my initialization");
+    }
+
+    @PreDestroy
+    public void doMyDestroy(){
+        System.out.println("Doing my destroy");
+    }
+
     @Override
     public String getSong() {
         return "Hungarian Rhapsody";
