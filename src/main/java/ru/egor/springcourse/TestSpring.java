4054b3c82e31d366ee0c1ed1e6fe2571329040b0
Egor Anchutin <zbit16@gmail.com>
2020-12-05 22:12:30 +0300

    Create SpringConfig an configure app with java code

--- src/main/java/ru/egor/springcourse/TestSpring.java
+++ src/main/java/ru/egor/springcourse/TestSpring.java
@@ -1,28 +1,15 @@
 package ru.egor.springcourse;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.awt.*;
 
 public class TestSpring {
     public static void main(String[] args) {
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
-                "applicationcontext.xml"
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(
+                SpringConfig.class
         );
-
-//        Music music1 = context.getBean("rockMusic", Music.class);
-//        MusicPlayer rockMusicPlayer = new MusicPlayer(music1);
-//        rockMusicPlayer.playMusic();
-//
-//        Music music2 = context.getBean("classicalMusic", Music.class);
-//        MusicPlayer classicalMusicPlayer = new MusicPlayer(music2);
-//        classicalMusicPlayer.playMusic();
-
-//        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-//        musicPlayer.playMusic();
-
-//        Computer computer = context.getBean("computer", Computer.class);
-//        System.out.println(computer);
 
         MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
 
