f09f35ef4e07c1315e92e738b285840d1dc9703b
Egor Anchutin <zbit16@gmail.com>
2020-12-05 20:21:25 +0300

    Learn about @Autowired

--- src/main/java/ru/egor/springcourse/TestSpring.java
+++ src/main/java/ru/egor/springcourse/TestSpring.java
@@ -2,19 +2,27 @@ package ru.egor.springcourse;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import java.awt.*;
+
 public class TestSpring {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
                 "applicationcontext.xml"
         );
 
-        Music music1 = context.getBean("rockMusic", Music.class);
-        MusicPlayer rockMusicPlayer = new MusicPlayer(music1);
-        rockMusicPlayer.playMusic();
+//        Music music1 = context.getBean("rockMusic", Music.class);
+//        MusicPlayer rockMusicPlayer = new MusicPlayer(music1);
+//        rockMusicPlayer.playMusic();
+//
+//        Music music2 = context.getBean("classicalMusic", Music.class);
+//        MusicPlayer classicalMusicPlayer = new MusicPlayer(music2);
+//        classicalMusicPlayer.playMusic();
 
-        Music music2 = context.getBean("classicalMusic", Music.class);
-        MusicPlayer classicalMusicPlayer = new MusicPlayer(music2);
-        classicalMusicPlayer.playMusic();
+//        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+//        musicPlayer.playMusic();
+
+        Computer computer = context.getBean("computer", Computer.class);
+        System.out.println(computer);
 
         context.close();
     }
